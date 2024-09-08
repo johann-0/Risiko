448d0cc5e0a1cde867819a2d3210b944a9f5a072
johann-macbook <90184788+johann-0@users.noreply.github.com>
2024-09-08 02:56:33 +0200

    version alpha

--- Risiko Game/Scripts/main_menu.gd
+++ Risiko Game/Scripts/main_menu.gd
@@ -4,14 +4,15 @@ func _ready():
 	$Join.connect("pressed", Callable(self, "_on_join_pressed"))
 	$Host.connect("pressed", Callable(self, "_on_host_pressed"))
 	
-	var defPlayer = GameData.Player.DEFAULT_PLAYER()
-	var defServerName = GameData.ServerName.DEFAULT_SERVER_NAME()
-	
-	GameData.players[GameData.localPlayerIndex]._name = defPlayer._name
-	GameData.serverName = defServerName
-	$PlayerName.text = defPlayer._name
-	$ServerAddress.text = defServerName._address
-	$ServerPort.text = str(defServerName._port)
+	if GameData.DEBUG_MODE == true:
+		var defPlayer = GameData.Player.DEFAULT_PLAYER()
+		var defServerName = GameData.ServerName.DEFAULT_SERVER_NAME()
+		
+		GameData.players[GameData.localPlayerIndex]._name = defPlayer._name
+		GameData.serverName = defServerName
+		$PlayerName.text = defPlayer._name
+		$ServerAddress.text = defServerName._address
+		$ServerPort.text = str(defServerName._port)
 	
 	if GameData.DEBUG_MODE == true:
 		await get_tree().create_timer(0.3).timeout
